bdab795cea565f3a7d2adba1896875c0eb30327a
matthewjlee <matthewjlee256@gmail.com>
2017-11-16 17:30:01 -0500

    working checkpoint 1

--- search_passthrough.py
+++ search_passthrough.py
@@ -5,13 +5,15 @@ import logging
 
 from concrete import AnnotationMetadata, ServiceInfo
 from concrete.search import SearchService
-from concrete.search.ttypes import SearchResult
+from concrete.search.ttypes import SearchResult, SearchCapability, SearchType
 from concrete.services.ttypes import ServicesException
-from concrete.util import AnalyticUUIDGeneratorFactory, SearchServiceWrapper
-from concrete.util.search_wrapper import SearchClientWrapper
+from concrete.util import AnalyticUUIDGeneratorFactory, SearchServiceWrapper, SearchClientWrapper
 
 
 class SearchHandler(SearchService.Iface):
+    def __init__(self, other, corpus_name):
+        self.other = other
+        self.corpus_name = corpus_name
     def alive(self):
         return True
 
@@ -19,15 +21,17 @@ class SearchHandler(SearchService.Iface):
         return ServiceInfo(name='search pass', version='0.0')
 
     def getCapabilities(self):
-        raise ServicesException()
+        return [SearchCapability(SearchType.SENTENCES)]
+        # raise ServicesException()
 
     def getCorpora(self):
-        raise ServicesException()
+        raise [self.corpus_name]
 
     def search(self, query):
-        augf = AnalyticUUIDGeneratorFactory()
-        aug = augf.create()
-        with SearchClientWrapper("localhost", "9090") as sc:
+        # return self.other.search(query)
+        # augf = AnalyticUUIDGeneratorFactory()
+        # aug = augf.create()
+        with SearchClientWrapper("search", "9090") as sc:
             return sc.search(query)
 
 
@@ -35,14 +39,19 @@ if __name__ == "__main__":
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("--port", type=int, default=9090)
+    parser.add_argument("-p", "--port", type=int, default=9090)
     parser.add_argument("--host", default="localhost")
+    parser.add_argument("--search_port", type=int, default=9090)
+    parser.add_argument("--search_host", default="localhost")
     args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)-15s %(levelname)s: %(message)s',
                         level='DEBUG')
-
-    handler = SearchHandler()
+    
+    # time.sleep(3000)
+    # with SearchClientWrapper("search", "9090") as search_client:
+    # handler = SearchHandler(search_client, "wikiQA")
+    handler = SearchHandler(None, "wikiQA")
 
     server = SearchServiceWrapper(handler)
 
