878fd877e6434e13189998895b5d9a4ed3447d47
Hannah Cowley <hcowley14@gmail.com>
2017-12-10 16:03:09 -0500

    FEAT: added resampling into training

--- train_model.py
+++ train_model.py
@@ -1,6 +1,8 @@
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.utils import resample
 import pickle
 import numpy as np
 from random import shuffle
@@ -35,7 +37,7 @@ def subtractive_balance(x, y):
     return (x[id_bal], y[id_bal])
 
 
-def train_model(train_data, dev_data):
+def train_model_SVM(train_data, dev_data, num_resamples=5):
     x_train = train_data['x']
     #x_train = x_train.reshape(-1, 1)
     y_train = train_data['y']
@@ -49,16 +51,18 @@ def train_model(train_data, dev_data):
     best_p = 0
     best_r = 0
     f1_scores = []
-    while (C <= 1e7):  
-        model = LinearSVC(C=C)
-        model.fit(x_train, y_train)
-        y_dev_pred = model.predict(x_dev)
-        f1 = f1_score(y_dev, y_dev_pred)
-        if f1 > best_f1:
-            best_f1 = f1
-            curr_best_C = C
-            best_p = precision_score(y_dev, y_dev_pred)
-            best_r = recall_score(y_dev, y_dev_pred)
+    while (C <= 1e7):
+        for sample_num in range(num_resamples):
+            model = LinearSVC(C=C)
+            (x_train_boot, y_train_boot) = resample(x_train, y_train)
+            model.fit(x_train_boot, y_train_boot)
+            y_dev_pred = model.predict(x_dev)
+            f1 = f1_score(y_dev, y_dev_pred)
+            if f1 > best_f1:
+                best_f1 = f1
+                curr_best_C = C
+                best_p = precision_score(y_dev, y_dev_pred)
+                best_r = recall_score(y_dev, y_dev_pred)
         C = C * 10
     print(best_f1)
     print(best_p)
@@ -66,10 +70,21 @@ def train_model(train_data, dev_data):
     print(curr_best_C)
 
 
+def train_MLP(train_data, dev_data):
+    x_train = train_data['x']
+    #x_train = x_train.reshape(-1, 1)
+    y_train = train_data['y']
+    (x_train, y_train) = subtractive_balance(x_train, y_train)
+    x_dev = dev_data['x']
+    #x_dev = x_dev.reshape(-1, 1)
+    y_dev = dev_data['y']
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+
+
 def main():
     train_data = pickle.load(open("./processed_train.p", "rb"))
     dev_data = pickle.load(open("./processed_dev.p", "rb"))
-    train_model(train_data, dev_data)
+    train_model_SVM(train_data, dev_data)
 
 
 if __name__ == '__main__':
