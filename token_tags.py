c107c8140d0556d8d7eee8aa59ad570814755c48
Jordan Peykar <jpeykar96@gmail.com>
2017-12-16 16:10:59 -0500

    FEAT: tags into dictionary

--- token_tags.py
+++ token_tags.py
@@ -22,110 +22,55 @@ def match_dict(match):
         for line in f:
             line = line.decode('UTF-8')
             arr = line.split("\t")
-            sid = arr[0]
-            caw_info = arr[2]
+            sid = str(arr[0])
+            caw_info = arr[2].encode('UTF-8')
             if caw_info not in sent_match.keys():
                 sent_match[caw_info] = [sid]
             else:
                 sent_match[caw_info].append(sid)
                 #print(sent_match[caw_info])
     s_match = OrderedDict(sorted(sent_match.items(), key=lambda i: i[0]))
+    print(s_match)
     return s_match
 
 
 def match_tags(match_dict):
-    mt = {}
+    sid_to_tokens = {}
     (caw_info, sid) = zip(*match_dict.items())
     caw_info = list(caw_info)
     sid = list(sid)
+    print(sid)
     with FetchCommunicationClientWrapper("ec2-35-153-184-225.compute-1.amazonaws.com", 9090) as fc:
         print("opened connection")
         comm_count = len(caw_info)
         start_count = 0
-        curr_caws = caw_info[start_count:1]
-        end_count = min(5 + end_count, comm_count - start_count)
-        print(curr_caws)
-        comm_ids = [i.split(':')[0] for i in curr_caws]
-        print(comm_ids)
-        fetchObj = FetchRequest(communicationIds=comm_ids)
-        print(fetchObj)
-        fr = fc.fetch(fetchObj)
-        print(fr)
-        comm_num = 0
-        for comm in fr.communications:
-            print("butts")
-            info = curr_caws[comm_num].split(':')
-            section_num = info[1]
-            sent_num = info[2]
-            section = lun(comm.sectionList)[section_num]
-            sentence = lun(section.sentenceList)[sent_num]
-            print(sentence)
-            for token_tag in get_tagged_tokens(sentence.tokenization, 'POS'):
-                print(token_tag)
-            comm_num += 1
-        start_count += 5
-        start_count = min(start_count, comm_count)
-
-
-def match_tags2(match_dict):
-    with FetchCommunicationClientWrapper("ec2-35-153-184-225.compute-1.amazonaws.com", 9090) as fc:
-        print("opened connection")
-        comm_count = fc.getCommunicationCount()
-        start_count = 0
-        while start_count != comm_count:
-            conn_comIDs = fc.getCommunicationIDs(
-                start_count, min(1, comm_count - start_count)
-            )
-            print(conn_comIDs)
-            fetchObj = FetchRequest(communicationIds=conn_comIDs)
-            fr = fc.fetch(fetchObj)
-        end_count = min(5, comm_count - start_count)
+        end_count = 5
+        sid_count = 0
         while end_count < comm_count:
-            curr_caws = caw_info[start_count:1]
-            end_count = min(5 + end_count, comm_count - start_count)
-            print(curr_caws)
+            curr_caws = caw_info[start_count:end_count]
             comm_ids = [i.split(':')[0] for i in curr_caws]
             print(comm_ids)
             fetchObj = FetchRequest(communicationIds=comm_ids)
-            print(fetchObj)
             fr = fc.fetch(fetchObj)
-            print(fr)
-            print(fr.communications)
-            comm_num = 0
+            comm_num = 0 
             for comm in fr.communications:
-                print(comm.id)
-                for section in lun(comm.sectionList):
-                    for sentence in lun(section.sentenceList):
-                        for token_tag in get_tagged_tokens(sentence.tokenization, 'POS'):
-                            print(token_tag)
-            start_count += 50
+                info = curr_caws[comm_num].split(':')
+                section_num = int(info[1])
+                sent_num = int(info[2])
+                section = lun(comm.sectionList)[section_num]
+                sentence = lun(section.sentenceList)[sent_num]
+                tokens = get_tokens(sentence.tokenization)
+                tags = get_tagged_tokens(sentence.tokenization, 'POS')
+                sid_to_tokens[sid[sid_count][0]] = tags
+                comm_num += 1
+                sid_count += 1
+            start_count += 5
             start_count = min(start_count, comm_count)
-            break
+            end_count = min(start_count + 5, comm_count -1)
+            print(sid_to_tokens)
+    return(sid_to_tokens)
 
 
-<<<<<<< HEAD
-=======
-def match_tags2(match_dict):
-    with FetchCommunicationClientWrapper("ec2-35-153-184-225.compute-1.amazonaws.com", 9090) as fc:
-        print("opened connection")
-        comm_count = fc.getCommunicationCount()
-        start_count = 0
-        while start_count != comm_count:
-            conn_comIDs = fc.getCommunicationIDs(
-                start_count, min(1, comm_count - start_count)
-            )
-            print(conn_comIDs)
-            fetchObj = FetchRequest(communicationIds=conn_comIDs)
-            fr = fc.fetch(fetchObj)
-            for comm in fr.communications:
-                print(comm.id)
-                for section in lun(comm.sectionList):
-                    for sentence in lun(section.sentenceList):
-                        for token_tag in get_tagged_tokens(sentence.tokenization, 'POS'):
-                            print(token_tag)
-            start_count += 50
-            start_count = min(start_count, comm_count)
->>>>>>> a6bdc33b8c5e65fdec5bab0e2f093a69a8e5ef23
 def main():
     matches = match_dict("./data/WikiQA-match/train-match.tsv")
     #print(matches)
