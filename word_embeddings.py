c3bb0190faeb55a7f6d183703269785321fd29f3
Jordan Peykar <jpeykar96@gmail.com>
2017-12-18 14:43:37 -0500

    added word embedding feature

--- word_embeddings.py
+++ word_embeddings.py
@@ -2,12 +2,45 @@ class WordEmbeddings:
     def __init__(self):
         from sklearn.metrics.pairwise import cosine_similarity
         import spacy
+        import numpy as np
         from numpy import array
+        from numpy import matrix
         self.nlp = spacy.load('en')
         self.cosine = cosine_similarity
         self.array = array
+        self.matrix = matrix
+        self.np = np
 
-    def getArray(self, query, answer):
+    def getDetVal(self, query, answer):
+        # query = query.replace(","," ")
+        # query = query.replace("'"," ")
+        # query = query.replace('"',"")
+        # query = query.replace("/"," ")
+        # query = query.replace("?","")
+        terms1 = self.nlp(query)
+        terms2 = self.nlp(answer)
+        A = []
+        for term in terms1:
+            if term.has_vector:
+                A.append(term.vector)
+        B = []
+        for term in terms2:
+            if term.has_vector:
+                B.append(term.vector)
+        A = self.np.matrix(A)
+        B = self.np.matrix(B)
+        A = A.transpose()
+        dotProd = self.np.dot(B, A)
+        dimMax = max(self.np.shape(dotProd))
+        dimMin = min(self.np.shape(dotProd))
+        detSum = 0
+        for i in range(dimMax - dimMin + 1):
+            sub = dotProd[i:i + dimMin, :]
+            detSum += self.np.linalg.det(sub)
+        detSum = detSum / (dimMax-dimMin+1)
+        return detSum
+
+    def getSumVal(self, query, answer):
         # query = query.replace(","," ")
         # query = query.replace("'"," ")
         # query = query.replace('"',"")
@@ -30,5 +63,8 @@ class WordEmbeddings:
         vector2 = self.array(vector2).reshape(1, -1)
         return self.cosine(vector1, vector2)[0][0]
 
-wordEm = WordEmbeddings()
-print(wordEm.getArray("hello world", "hello one of us")) 
+# wordEm = WordEmbeddings()
+# print(wordEm.getDetVal("hello world", "hello one of us"))
+# print(wordEm.getDetVal("hello world", "hello world"))  
+# print(wordEm.getSumVal("hello world", "hello one of us")) 
+# print(wordEm.getSumVal("hello world", "hello world"))
