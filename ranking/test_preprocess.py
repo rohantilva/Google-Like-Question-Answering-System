8e818d25f26c0a4776e6e4f754edb7c0b2e2397b
Hannah Cowley <hcowley14@gmail.com>
2017-12-19 15:10:24 -0500

    FEAT: integrating feature stuff

--- ranking/test_preprocess.py
+++ ranking/test_preprocess.py
@@ -1,14 +1,14 @@
 from preprocess import Preprocess
 
 
-def test_build_dataset(datapath, pickle_name):
+def build_dataset(datapath, pickle_name):
     p = Preprocess()
     p.process_dataset(datapath, pickle_name)
 
 
 def main():
-    test_build_dataset("../data/WikiQA/WikiQA-train.tsv.gz", "processed_train")
-
+    build_dataset("../data/WikiQA/WikiQA-dev.tsv.gz", "processed_dev")
+    build_dataset("../data/WikiQA/WikiQA-test.tsv.gz", "processed_test")
 
 if __name__ == '__main__':
     main()
